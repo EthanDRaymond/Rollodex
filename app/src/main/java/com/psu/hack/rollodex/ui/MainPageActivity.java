a5447b6a47526804e0bf6a73674ad55900a835f2
eraymond <e.raymond@comcast.net>
2015-11-08 03:24:49 -0500

    UIs are now merged.

--- app/src/main/java/com/psu/hack/rollodex/ui/MainPageActivity.java
+++ app/src/main/java/com/psu/hack/rollodex/ui/MainPageActivity.java
@@ -4,7 +4,16 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,7 +32,8 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainPageActivity extends Activity {
+public class MainPageActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private RelativeLayout mainBox;
     private LinearLayout cardList;
@@ -35,8 +45,30 @@ public class MainPageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), ShareActivity.class));
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         this.mainBox = (RelativeLayout) findViewById(R.id.main_page_mainBox);
         this.cardList = (LinearLayout) findViewById(R.id.main_page_card_list);
         this.cardViews = new ArrayList<>();
@@ -47,18 +79,70 @@ public class MainPageActivity extends Activity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        if (deleteMode) {
-            leaveDeleteMode(false);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
         } else {
-            try {
-                ContactList.writeContactsToFile(this);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+            super.onBackPressed();
+            if (deleteMode) {
+                leaveDeleteMode(false);
+            } else {
+                try {
+                    ContactList.writeContactsToFile(this);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finish();
             }
-            finish();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_gallery) {
+
+
+        } else if (id == R.id.nav_manage) {
+
+            startActivity(new Intent(getBaseContext(),SettingsActivity.class));
+        } else if (id == R.id.nav_share) {
+            startActivity(new Intent(getBaseContext(),ShareActivity.class));
+        }
+        else if (id == R.id.nav_view){
+            startActivity(new Intent(getBaseContext(),AboutActivity.class));
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     public void buildCardList() {
---
+++ app/src/main/java/com/psu/hack/rollodex/ui/ShareActivity.java
@@ -0,0 +1,15 @@
+package com.psu.hack.rollodex.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.psu.hack.rollodex.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
