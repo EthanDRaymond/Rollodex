60f788959d3d21316677a77e3fc12a74a6f7e600
eraymond <e.raymond@comcast.net>
2015-11-07 22:26:18 -0500

    Fixed bug where parameters were not being added to the user card.

--- app/src/main/java/com/psu/hack/rollodex/card/UserCard.java
+++ app/src/main/java/com/psu/hack/rollodex/card/UserCard.java
@@ -4,9 +4,13 @@ package com.psu.hack.rollodex.card;
  * Created by matt on 11/7/2015.
  */
 public class UserCard extends Card {
+
     private static String name, phoneNumber, emailAddress;
-    public UserCard(String name, String phoneNumber, String emailAddress)
-    {
+
+    public UserCard(String name, String phoneNumber, String emailAddress) {
         super( name,  phoneNumber,  emailAddress);
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
     }
 }
