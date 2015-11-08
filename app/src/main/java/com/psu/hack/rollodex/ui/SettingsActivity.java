7752f79a2b2d42205ae993fcb51f83a7f9b50545
eraymond <e.raymond@comcast.net>
2015-11-08 04:01:41 -0500

    Added new settings activity.

--- app/src/main/java/com/psu/hack/rollodex/ui/SettingsActivity.java
+++ app/src/main/java/com/psu/hack/rollodex/ui/SettingsActivity.java
@@ -1,11 +1,14 @@
 package com.psu.hack.rollodex.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.psu.hack.rollodex.R;
 
@@ -15,17 +18,22 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Spinner spin = (Spinner) findViewById(R.id.spin);
+        ArrayAdapter<CharSequence> adapters = ArrayAdapter.createFromResource(this,R.array.Options,android.R.layout.simple_spinner_dropdown_item);
+        adapters.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spin.setAdapter(adapters);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(getBaseContext(), ShareActivity.class));
             }
         });
+
     }
 
 }
